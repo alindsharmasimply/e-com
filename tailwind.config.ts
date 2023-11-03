3869bcde902579ae96f112f144ad7f1cbd36d1a7
alindsharmasimply <alindsharmasimply@gmail.com>
2023-11-03 21:50:09 +0530

    [Alind] Create an Add Product Page

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,34 +1,34 @@
-import type { Config } from 'tailwindcss';
+import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
     },
   },
-  plugins: [require('daisyui')],
+  plugins: [require("daisyui")],
   daisyui: {
     themes: [
       {
         mytheme: {
-          primary: '#d8c6ff',
-          secondary: '#095184',
-          accent: '#e2c166',
-          neutral: '#271e33',
-          'base-100': '#403a40',
-          info: '#81a0ef',
-          success: '#5be1c8',
-          warning: '#efbd57',
-          error: '#f82420',
+          primary: "#50d379",
+          secondary: "#fcbeb0",
+          accent: "#128d96",
+          neutral: "#2d2537",
+          "base-100": "#f8f5fa",
+          info: "#acc0ec",
+          success: "#19ccab",
+          warning: "#bd7005",
+          error: "#ee1753",
         },
       },
     ],
