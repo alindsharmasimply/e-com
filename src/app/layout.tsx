3a1097ed1fcfa1daa8f962b8a028254b09b1e8eb
alindsharmasimply <alindsharmasimply@gmail.com>
2023-11-05 19:36:44 +0530

    [Alind] Created Navbar and Footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Navbar from "./navbar/Navbar";
+import Footer from "./Footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,9 +19,11 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <main className="min-w-min-[300px] m-auto max-w-7xl p-5">
+        <Navbar />
+        <main className="min-w-min-[300px] m-auto max-w-7xl p-4">
           {children}
         </main>
+        <Footer />
       </body>
     </html>
   );
